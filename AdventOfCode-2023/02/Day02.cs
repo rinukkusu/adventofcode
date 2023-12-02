e3a6bab2b250fa10172613813f76485389b91114
Maximilian Riegler <mail@maxriegler.at>
2023-12-02 16:21:13 +0100

    add day 2

---
+++ AdventOfCode-2023/02/Day02.cs
@@ -0,0 +1,34 @@
+﻿namespace AdventOfCode_2023._02;
+
+public class Day02
+{
+    [Theory]
+    [InlineData(Data.Example01, 12, 13, 14, 8)]
+    [InlineData(Data.PuzzleInput, 12, 13, 14, 2593)]
+    public void Test1(string data, int maxRed, int maxGreen, int maxBlue, int expectedResult)
+    {
+        var games = data.Trim()
+            .Split(Environment.NewLine)
+            .Select(Game.FromString);
+        
+        var result = games.Where(x => x.Red <= maxRed && x.Green <= maxGreen && x.Blue <= maxBlue)
+            .Sum(x => x.Id);
+        
+        Assert.Equal(expectedResult, result);
+    }
+    
+    [Theory]
+    [InlineData(Data.Example01, 2286)]
+    [InlineData(Data.PuzzleInput, 54699)]
+    public void Test2(string data, int expectedResult)
+    {
+        var games = data.Trim()
+            .Split(Environment.NewLine)
+            .Select(Game.FromString);
+        
+        var result = games
+            .Sum(x => x.Red * x.Green * x.Blue);
+        
+        Assert.Equal(expectedResult, result);
+    }
+}
---
+++ AdventOfCode-2023/02/Game.cs
@@ -0,0 +1,43 @@
+﻿namespace AdventOfCode_2023._02;
+
+public class Game(int id, int red, int green, int blue)
+{
+    public int Id { get; } = id;
+    public int Red { get; } = red;
+    public int Green { get; } = green;
+    public int Blue { get; } = blue;
+
+    public static Game FromString(string line)
+    {
+        var gameSplit = line.Split(":");
+
+        var id = int.Parse(gameSplit[0].Trim().Split(" ")[1]);
+        var red = 0;
+        var green = 0;
+        var blue = 0;
+
+        var rounds = gameSplit[1].Split(";");
+        foreach (var round in rounds)
+        {
+            var draws = round.Split(",");
+            foreach (var draw in draws)
+            {
+                var drawSplit = draw.Trim().Split(" ");
+                var cubeCount = int.Parse(drawSplit[0]);
+                var cubeColor = drawSplit[1];
+
+                if (cubeColor == "red")
+                    red = cubeCount > red ? cubeCount : red;
+                else if (cubeColor == "green")
+                    green = cubeCount > green ? cubeCount : green;
+                else if (cubeColor == "blue")
+                    blue = cubeCount > blue ? cubeCount : blue;
+                else
+                    throw new Exception(@$"What is this color '{cubeColor}'?
+{line}");
+            }
+        }
+
+        return new Game(id, red, green, blue);
+    }
+}
